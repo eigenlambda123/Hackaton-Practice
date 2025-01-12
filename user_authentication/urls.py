ed3ff59efec2e87fae8c1d70f39a092ca60e461d
Rodmark "RM" Villa <rmvilla987@gmail.com>
2025-01-12 11:06:36 +0800

    Add user registration functionality with form and view

--- user_authentication/urls.py
+++ user_authentication/urls.py
@@ -1,6 +1,9 @@
 from . import views
 from django.urls import path
 
+app_name = 'authentication'  # This defines the namespace
+
 urlpatterns = [
     path('login/', views.user_login, name='login'),
+    path('register/', views.user_register, name='register'),
 ]
--- user_authentication/views.py
+++ user_authentication/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import authenticate, login
-from django.contrib import messages
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 # Create your views here.
 def user_login(request):
@@ -18,5 +19,35 @@ def user_login(request):
             # If authentication is successful
             login(request, user)
             return redirect('map')
-
     return render(request, 'user_authentication/login.html')
+
+
+def user_register(request):
+    if request.method == "POST":
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        email = request.POST.get('email')
+        
+        # Check if the user already exists
+        user = User.objects.filter(username=username)
+
+        if user.exists():
+            messages.info(request, 'Username already exists')
+            return redirect('register')
+        
+        # Create A new User Object with the provided Info
+        user = User.objects.create_user(first_name=first_name,
+                                        last_name=last_name,
+                                        username=username,
+                                        password=password,
+                                        email=email)
+        # Save user password and object
+        user.set_password(password)
+        user.save()
+
+        messages.info(request, 'Account Created Successfully!')
+        return redirect('register')
+    
+    return render(request, 'user_authentication/register.html')

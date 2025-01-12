7e7879d7c036577a0a5bef3d8cd4d88316648b38
Rodmark "RM" Villa <rmvilla987@gmail.com>
2025-01-12 10:23:07 +0800

    Add user authentication URLs and login template

--- ar_walkability/urls.py
+++ ar_walkability/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('guide.urls')),
+    path('authentication/', include('user_authentication.urls')),
 ]
--- user_authentication/views.py
+++ user_authentication/views.py
@@ -1,9 +1,9 @@
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, login
 from django.contrib import messages
 from django.shortcuts import render, redirect
 
 # Create your views here.
-def login(request):
+def user_login(request):
     if request.method == "POST":
         username = request.POST.get('username')
         password = request.POST.get('password')
@@ -16,7 +16,7 @@ def login(request):
             return redirect('login')
         else:
             # If authentication is successful
-            login(request,user)
-            return redirect('map_view')
+            login(request, user)
+            return redirect('map')
 
-    return render(request, 'login.html')
+    return render(request, 'user_authentication/login.html')

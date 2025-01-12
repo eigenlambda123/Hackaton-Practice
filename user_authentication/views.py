4e8d78484265c1e6265015198c93e353dea540b0
Rodmark "RM" Villa <rmvilla987@gmail.com>
2025-01-12 10:07:45 +0800

    Add login view with authentication and error handling

--- user_authentication/views.py
+++ user_authentication/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate
+from django.contrib import messages
+from django.shortcuts import render, redirect
 
 # Create your views here.
+def login(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user is None:
+            # If authentication failed 
+            messages.error(request, 'Invalid Login')
+            return redirect('login')
+        else:
+            # If authentication is successful
+            login(request,user)
+            return redirect('map_view')
+
+    return render(request, 'login.html')
